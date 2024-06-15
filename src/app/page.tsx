1cfcd7eebe7923ec3562ffe4e20be489bb585321
Anjan Shomodder <thatanjan@gmail.com>
2024-06-15 17:21:21 +0600

    Add heading to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <main>Game Search</main>
+  return (
+    <main>
+      <div className="mx-auto max-w-[800px] px-5">
+        <h1 className="mb-10 mt-[200px] text-center text-7xl font-bold">
+          Game Search
+        </h1>
+      </div>
+      h
+    </main>
+  )
 }
 
 export default Home
