8f3180812b286bd89072495256c9b33cedac44b6
Anjan Shomodder <thatanjan@gmail.com>
2024-06-15 18:15:21 +0600

    Display the highlighted text with a background

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -15,7 +15,7 @@ const SearchResults = ({ query }: Props) => {
     <div className="max-h-[370px] overflow-y-auto">
       {!!results.length && <hr className="bg-gray-200 hover:bg-gray-800" />}
 
-      {results.map(({ title, release_year, logo, highlightedTitle }) => (
+      {results.map(({ title, release_year, logo, highlightedTitle = [] }) => (
         <Link key={title} href={`/game/${title}`}>
           <div className="flex gap-4 px-5 py-3 hover:bg-gray-500">
             <Image
@@ -26,7 +26,13 @@ const SearchResults = ({ query }: Props) => {
               loader={({ src }) => src}
               className="w-5"
             />
-            <h2 className="grow truncate">{highlightedTitle}</h2>
+            <h2 className="grow truncate">
+              {highlightedTitle.map(({ text, matched }) => (
+                <span className={matched ? 'bg-gray-600' : ''} key={text}>
+                  {text}
+                </span>
+              ))}
+            </h2>
 
             <span className="shrink-0">{release_year}</span>
           </div>
