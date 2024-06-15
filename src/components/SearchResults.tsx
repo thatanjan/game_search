05960481e9d266a51811828fe7678f0e90504fc0
Anjan Shomodder <thatanjan@gmail.com>
2024-06-15 19:27:12 +0600

    Move the search result item to a new component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,6 +1,5 @@
 import { Game } from '@/types/games'
 import Link from 'next/link'
-import Image from 'next/image'
 import fuzzySearch from '@/utils/search'
 import data from '@/data.json'
 import NoResult from './NoResult'
@@ -11,6 +10,28 @@ import GameLogo from './GameLogo'
 interface Props {
   query: string
 }
+
+const SearchResultItem = ({
+  title,
+  release_year,
+  logo,
+  highlightedTitle = [],
+}: Game) => (
+  <Link href={`/game/${title}`}>
+    <div className="flex gap-4 px-5 py-3 hover:bg-gray-500">
+      <GameLogo title={title} logo={logo} />
+      <h2 className="grow truncate">
+        {highlightedTitle.map(({ text, matched }) => (
+          <span className={matched ? 'bg-gray-600' : ''} key={text}>
+            {text}
+          </span>
+        ))}
+      </h2>
+
+      <span className="shrink-0">{release_year}</span>
+    </div>
+  </Link>
+)
 
 const SearchResults = ({ query }: Props) => {
   const [results, setResults] = useState<Game[]>([])
@@ -30,21 +51,8 @@ const SearchResults = ({ query }: Props) => {
     <>
       {!!results.length && <hr className="bg-gray-200" />}
       <div className="max-h-[370px] overflow-y-auto">
-        {results.map(({ title, release_year, logo, highlightedTitle = [] }) => (
-          <Link key={title} href={`/game/${title}`}>
-            <div className="flex gap-4 px-5 py-3 hover:bg-gray-500">
-              <GameLogo title={title} logo={logo} />
-              <h2 className="grow truncate">
-                {highlightedTitle.map(({ text, matched }) => (
-                  <span className={matched ? 'bg-gray-600' : ''} key={text}>
-                    {text}
-                  </span>
-                ))}
-              </h2>
-
-              <span className="shrink-0">{release_year}</span>
-            </div>
-          </Link>
+        {results.map((game) => (
+          <SearchResultItem {...game} key={game.title} />
         ))}
 
         {!results.length && <NoResult />}
